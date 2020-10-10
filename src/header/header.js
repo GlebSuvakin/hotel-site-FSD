702c4d78caefc1dbf926cd7f52f46a4a9d5965bf
Gleb <iitikbo0216suvakin.g@gmail.com>
2020-10-10 11:57:20 +0300

    Create header pug template

--- src/header/header.js
+++ src/header/header.js
@@ -1,1 +1,11 @@
 import './header.scss';
+
+import './__logo-group/header__logo-group.scss';
+
+import './__nav-group/header__nav-group.scss';
+
+import './__nav-link/header__nav-link.scss';
+
+import './__btn-group/header__btn-group.scss';
+
+import './__btn/header__btn.scss';
